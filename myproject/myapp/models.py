b9bede026a8e578117929be39e160415e13afd08
Nghia31_01 <86704345+UhuGiHan@users.noreply.github.com>
2024-11-20 06:34:21 +0700

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
